12e0aabcd33382e54c61b5cd6efc68c7a1fe185d
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2023-09-03 05:09:12 -0700

    Gave custom port + Better compatability

--- verify.py
+++ verify.py
@@ -4,12 +4,7 @@ from fastapi.responses import FileResponse
 from pydantic import BaseModel, EmailStr
 import dns.resolver
 from disposable_email_domains import blocklist
-# import asyncio
-# from hypercorn.config import Config
-# from hypercorn.asyncio import serve
 
-# import smtplib
-# import socket
 
 async def email_deduplication_and_spam_trap_removal(email: EmailStr, domain: str) -> Tuple[bool, str]:
     if domain in blocklist:
@@ -40,57 +35,11 @@ async def mta_validation(email: EmailStr, domain: str) -> Tuple[bool, str]:
     except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
         return False, "MX record not found for the domain."
 
-'''
-async def check_email_deliverability(email: EmailStr, domain: str):
-    
-    # Perform DNS lookup for MX records
-    try:
-        mx_records = dns.resolver.resolve(domain, 'MX')
-    except dns.resolver.NXDOMAIN:
-        return False, "MX record not found for the domain."
-    
-    # Sort MX records by preference
-    mx_records = sorted(mx_records, key=lambda record: record.preference)  # type: ignore
-    
-    # Perform SMTP handshake
-    for mx_record in mx_records:
-        print(mx_record)
-        mx_server = str(mx_record.exchange)
-        
-        try:
-            # Connect to the mail server
-            server = smtplib.SMTP(host=mx_server, timeout=3)
-            
-            # Start TLS for security
-            server.starttls()
-            
-            # SMTP 'helo' command
-            server.helo()
-            
-            # SMTP 'mail from' command
-            server.mail('')
-            
-            # SMTP 'rcpt to' command
-            code, message = server.rcpt(email)
-            
-            # Close the connection
-            server.quit()
-            
-            # Check if the server is willing to accept mail
-            if code == 250:
-                return True, "Email is deliverable."
-            else:
-                return False, "Email is not deliverable."
-        except (smtplib.SMTPConnectError, smtplib.SMTPServerDisconnected, socket.timeout):
-            continue
-    
-    return False, "Could not connect to any mail server."
-'''
 
 
 ##### START HERE########
 class Email(BaseModel):
-    name: EmailStr
+    email: EmailStr
 
 app = FastAPI()
 
@@ -110,14 +59,13 @@ async def verify_email(email: Email) -> dict:
         domain_validation,
         risk_validation,
         mta_validation,
-        #check_email_deliverability
+        # check_email_deliverability
     ]
-    domain = email.name.split('@')[1]
+    domain = email.email.split('@')[1]
     for step in steps:
-        is_valid, message = await step(email.name, domain)
+        is_valid, message = await step(email.email, domain)
         if not is_valid:
-            return {"is_valid": False, "message": message}
+            return {"result": "invalid", "message": message}
     
-    return {"is_valid": True, "message": "Email is valid."}
+    return {"result": "valid", "message": "Email is valid."}
 
-# asyncio.run(serve(app, Config())) # type: ignore
